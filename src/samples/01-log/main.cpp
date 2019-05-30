c0bc079ef109d154157bcfc9b240627b389e89b0
Sebastian Kylander <gaztinerag@gmail.com>
2019-05-30 17:27:01 +0200

    Add support for loading several movies in sample

--- src/samples/01-log/main.cpp
+++ src/samples/01-log/main.cpp
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <initializer_list>
+#include <string_view>
+
 #include <swf/swf.h>
 
 int main( int /*argc*/, char* /*argv*/[] )
 {
-	const char* filepath = "image.swf";
+	for( const std::string_view filepath : { "image.swf" } ) // <-- Add file paths here
+	{
+		printf( "~~ %s ~~\n", filepath.data() );
 
-	printf( "~~ %s ~~\n", filepath );
+		swf_movie movie;
+		if( swf_load( filepath.data(), &movie ) < 0 )
+		{
+			printf( "  FAILED TO LOAD\n" );
+			return -1;
+		}
 
-	swf_movie movie;
-	if( swf_load( filepath, &movie ) < 0 )
-		return -1;
-
-	printf( "Frame size:  %d x %d\n", movie.frameWidth, movie.frameHeight );
-	printf( "Frame count: %d\n", movie.frameCount );
-	printf( "Frame rate:  %.2f\n", movie.frameRate );
-	printf( "Tag count:   %d\n", movie.tagCount );
+		printf( "  Frame size:  %d x %d\n", movie.frameWidth, movie.frameHeight );
+		printf( "  Frame count: %d\n", movie.frameCount );
+		printf( "  Frame rate:  %.2f\n", movie.frameRate );
+		printf( "  Tag count:   %d\n", movie.tagCount );
+		printf( "\n" );
+	}
 
 	getchar();
 	return 0;
