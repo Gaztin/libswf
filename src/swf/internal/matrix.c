4961d834a0d59597c883dea410cad930efb2ff7c
Sebastian Kylander <developer@gaztin.com>
2020-01-12 14:33:51 +0100

    Implement matrix struct from file spec

---
+++ src/swf/internal/matrix.c
@@ -0,0 +1,50 @@
+/*
+* Copyright (c) 2020 Sebastian Kylander https://gaztin.com/
+*
+* This software is provided 'as-is', without any express or implied warranty. In no event will
+* the authors be held liable for any damages arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose, including commercial
+* applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not claim that you wrote the
+*    original software. If you use this software in a product, an acknowledgment in the product
+*    documentation would be appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+*    being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "matrix.h"
+
+#include "internal/reader.h"
+
+#include <stdint.h>
+#include <string.h>
+
+int swf_matrix__parse( swf_reader* rd, swf_matrix* outMatrix )
+{
+	outMatrix = outMatrix;
+
+	uint8_t hasScale;
+	if( swf_reader__read_bytes( rd, &hasScale, sizeof( hasScale ) ) < 0 )
+		return -1;
+
+	if( hasScale == 1 )
+	{
+		uint8_t nScaleBits[ 5 ];
+		if( swf_reader__read_bytes( rd, nScaleBits, sizeof( nScaleBits ) ) < 0 )
+			return -1;
+
+	}
+
+	return 0;
+}
+
+void swf_matrix__identity( swf_matrix* matrix )
+{
+	memset( matrix->elements, 0, sizeof( float ) * 6 );
+
+	matrix->elements[ 0 ] = 1.0f;
+	matrix->elements[ 3 ] = 1.0f;
+}
